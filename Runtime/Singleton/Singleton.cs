3da3a160996c0c1f78b5cde884a1e89127dae2bd
williamstrandp <strandenpaurell@outlook.com>
2024-02-27 09:47:15 +0100

    Added singleton class

---
+++ Runtime/Singleton/Singleton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Utilities.Singleton
+{
+    public class Singleton<T> where T : MonoBehaviour
+    {
+        static T instance;
+        
+        public static T Instance
+        {
+            get
+            {
+                if (instance != null) return instance;
+                
+                instance = Object.FindObjectOfType<T>();
+                
+                if (instance != null) return instance;
+                
+                instance = new GameObject(typeof(T).Name).AddComponent<T>();
+                return instance;
+            }
+        }
+    }
+}
